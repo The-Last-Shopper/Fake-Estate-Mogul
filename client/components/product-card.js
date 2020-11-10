3372b592e1eafbf17bdd8d30ecc9e30e9357a9e5
LuFrei <lucasfreitas09876@gmail.com>
2020-11-10 12:57:20 -0500

    All-Products View & Product-Card components finished

---
+++ client/components/product-card.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
+
+export default props => {
+  return (
+    <div className="product-card">
+      <img src={props.product.image} />
+      <Link to={`/products/${props.product.id}`}>
+        <h4>{props.product.name}</h4>
+      </Link>
+      <p className="price">{props.product.price}</p>
+      <button type="button">Add To Cart</button>
+    </div>
+  )
+}
