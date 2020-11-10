9f94001cb1c8dbbd8e804aff6b4c7c7bdce10d3f
Valentyna Vasylieva <smithaddaf@gmail.com>
2020-11-10 16:48:35 -0600

    connected backend with frontend for All Products

--- client/components/product-card.js
+++ client/components/product-card.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 export default props => {
   return (
     <div className="product-card">
-      <img src={props.product.image} />
+      <img src={props.product.imageUrl} />
       <Link to={`/products/${props.product.id}`}>
         <h4>{props.product.name}</h4>
       </Link>
